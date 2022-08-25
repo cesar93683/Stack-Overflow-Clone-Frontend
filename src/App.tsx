782d3b9fb07d66ce55d4015699a261d966e4f588
Cesar <48188267+cesar93683@users.noreply.github.com>
2022-08-25 06:31:02 -0700

    Add router, Add LogIn and SignUp pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
-function App() {
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from "./pages/Home";
+import LogIn from "./pages/LogIn";
+import SignUp from "./pages/SignUp";
+
+
+export default function BasicExample() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Log In</Link>
+          </li>
+          <li>
+            <Link to="/signup">Sign Up</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/login" element={<LogIn />}/>
+          <Route path="/signup" element={<SignUp />}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/pages/LogIn.tsx
@@ -0,0 +1,8 @@
+export default function SignUp() {
+
+  return (
+    <div>
+        Log In Page
+    </div>
+  );
+}
