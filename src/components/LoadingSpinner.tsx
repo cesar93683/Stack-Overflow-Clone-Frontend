eef074a3ce4aea0698e1ecfc624947ec8e0ba916
Cesar <48188267+cesar93683@users.noreply.github.com>
2022-08-26 15:19:28 -0700

    A loading spinner now appears while Posts are being fetched

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,10 @@
+import { Spinner } from "react-bootstrap";
+
+export default function LoadingSpinner() {
+  return (
+    <div className="d-flex justify-content-center">
+      <Spinner animation="border" role="status">
+      </Spinner>
+    </div>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,40 @@
 import { useEffect, useState } from "react";
 import PostsService from "../service/PostsService";
 import IPost from "../interfaces/IPost";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 export default function Home() {
   const [posts, setPosts] = useState<IPost[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     PostsService.getAllPosts().then(
       data => {
         setPosts(data)
+        setLoading(false)
       }, error => {
-        // TODO
+        setLoading(false)
+        setError(true)
       })
   }, []);
+
+  if (loading) {
+    return <LoadingSpinner />
+  }
+
+  if (error) {
+    return <div>An error has occured</div>
+  }
+
+  if (posts.length === 0) {
+    return <div>There are no posts</div>
+  }
 
   return (
     <div>
       {posts.map((post, i) => 
-      <div>
+      <div key={i}>
       <div>{post.id}</div>
         <div>{post.title}</div>
         <div>{post.content}</div>
