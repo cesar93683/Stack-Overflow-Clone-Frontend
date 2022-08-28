5b56fa2c7c9fd9ed9e04d23625916b3d9c4434fc
Cesar <48188267+cesar93683@users.noreply.github.com>
2022-08-28 13:44:35 -0700

    Add services to PostService

--- src/service/PostsService.ts
+++ src/service/PostsService.ts
@@ -6,17 +6,57 @@ import IGenericResponse from '../utils/interfaces/service/IGenericResponse';
 const API_URL = 'http://localhost:8080/api/posts/';
 
 class PostsService {
-  async getAllPosts(page: number, sortedByVotes: boolean, headers: IHeaders) {
+  async getPosts(page: number, sortedByVotes: boolean, headers: IHeaders) {
     const response = await axios.get<IPost[]>(
       API_URL + 'all?page=' + page + '&sortedByVotes=' + sortedByVotes,
       headers
     );
     return response.data;
   }
+
+  async getPostsByUserId(
+    userId: number,
+    page: number,
+    sortedByVotes: boolean,
+    headers: IHeaders
+  ) {
+    const response = await axios.get<IPost[]>(
+      API_URL +
+        'users/' +
+        userId +
+        '?page=' +
+        page +
+        '&sortedByVotes=' +
+        sortedByVotes,
+      headers
+    );
+    return response.data;
+  }
+
   async getPost(id: string, headers: IHeaders) {
     const response = await axios.get<IPost>(API_URL + id, headers);
     return response.data;
   }
+
+  async getPostsResponses(
+    postId: number,
+    page: number,
+    sortedByVotes: boolean,
+    headers: IHeaders
+  ) {
+    const response = await axios.get<IPost[]>(
+      API_URL +
+        'responses/' +
+        postId +
+        '?page=' +
+        page +
+        '&sortedByVotes=' +
+        sortedByVotes,
+      headers
+    );
+    return response.data;
+  }
+
   async votePost(postId: string, action: string, headers: IHeaders) {
     const response = await axios.post<IGenericResponse>(
       API_URL + 'vote',
