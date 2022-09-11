a78857c9ca8e7cd2f6f15de76a812fd8ffceda25
cesar93683 <48188267+cesar93683@users.noreply.github.com>
2022-09-11 10:02:58 -0700

    Add sytax highlighting

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,4 +1,5 @@
 import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 
 interface ContentProps {
   content: string;
@@ -6,5 +7,26 @@ interface ContentProps {
 }
 
 export default function Content({ content, className }: ContentProps) {
-  return <ReactMarkdown children={content} className={className} />;
+  return (
+    <ReactMarkdown
+      children={content}
+      className={className}
+      components={{
+        code({ node, inline, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || '');
+          return !inline && match ? (
+            <SyntaxHighlighter
+              language={match[1]}
+              children={String(children).replace(/\n$/, '')}
+              PreTag="div"
+            />
+          ) : (
+            <code className={className} {...props}>
+              {children}
+            </code>
+          );
+        },
+      }}
+    />
+  );
 }
