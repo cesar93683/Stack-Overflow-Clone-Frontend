abf90ef02486a5cd4388538bbcf5605dbd2ec8d2
cesar93683 <48188267+cesar93683@users.noreply.github.com>
2022-09-10 17:21:57 -0700

    Content now uses react-markdown

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,50 +1,10 @@
+import ReactMarkdown from 'react-markdown';
+
 interface ContentProps {
   content: string;
   className?: string;
 }
 
-interface ContentPart {
-  isCode: boolean;
-  content: string;
-}
-
 export default function Content({ content, className }: ContentProps) {
-  let startIndex = 0;
-  let isCode = false;
-  const contentParts: Array<ContentPart> = [];
-  for (let i = 0; i < content.length - 2; i++) {
-    if (content.substring(i, i + 3) == '```') {
-      contentParts.push({
-        isCode,
-        content: content.substring(startIndex, i),
-      });
-      isCode = !isCode;
-      startIndex = i + 3;
-    }
-  }
-  if (startIndex !== content.length) {
-    contentParts.push({ isCode, content: content.substring(startIndex) });
-  }
-
-  return (
-    <div className={className}>
-      {contentParts.map((part, i) =>
-        part.isCode ? (
-          <div className="p-2 bg-light-gray" key={i}>
-            {part.content.split('\n').map((content, j) => (
-              <p key={j} className="mb-1">
-                {content}
-              </p>
-            ))}
-          </div>
-        ) : (
-          part.content.split('\n').map((content, j) => (
-            <p key={j} className="mb-1">
-              {content}
-            </p>
-          ))
-        )
-      )}
-    </div>
-  );
+  return <ReactMarkdown children={content} className={className} />;
 }
