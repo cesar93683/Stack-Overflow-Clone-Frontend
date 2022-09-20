e30f5379e311ed12e1b19503020ab51af401b547
cesar93683 <48188267+cesar93683@users.noreply.github.com>
2022-09-19 17:54:27 -0700

    Refactor QuestionForm to it's own component

---
+++ src/components/QuestionForm.tsx
@@ -0,0 +1,120 @@
+import { Alert, Button, CloseButton, Form } from 'react-bootstrap';
+import SelectSearch, {
+  fuzzySearch,
+  SelectedOptionValue,
+} from 'react-select-search';
+import 'react-select-search/style.css';
+import Content from './Content';
+import LoadingSpinner from './LoadingSpinner';
+
+interface QuestionFormTags {
+  onSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+  allTags: string[];
+  setTags: React.Dispatch<React.SetStateAction<string[]>>;
+  setAllTags: React.Dispatch<React.SetStateAction<string[]>>;
+  title: string;
+  onTitleChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  content: string;
+  onContentChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  tags: string[];
+  error: string;
+  submitLoading: boolean;
+}
+
+export default function QuestionForm(props: QuestionFormTags) {
+  const {
+    onSubmit,
+    allTags,
+    setTags,
+    setAllTags,
+    title,
+    onTitleChange,
+    content,
+    onContentChange,
+    tags,
+    error,
+    submitLoading,
+  } = props;
+
+  const getOptionTags = () => {
+    return allTags.map((tag) => ({ name: tag, value: tag }));
+  };
+
+  const onSelectTag = (tag: SelectedOptionValue | SelectedOptionValue[]) => {
+    if (typeof tag === 'string') {
+      setTags((value) => [...value, tag]);
+      setAllTags((value) => value.filter((item) => item !== tag));
+    }
+  };
+
+  const removeTag = (tag: string) => {
+    setTags((value) => value.filter((item) => item !== tag));
+    setAllTags((value) => [...value, tag]);
+  };
+
+  return (
+    <Form onSubmit={onSubmit}>
+      <Form.Group>
+        <Form.Label>Title</Form.Label>
+        <Form.Control
+          minLength={3}
+          maxLength={50}
+          type="text"
+          placeholder="Enter title"
+          value={title}
+          disabled={!onTitleChange}
+          onChange={onTitleChange}
+        />
+      </Form.Group>
+      <Form.Group className="mb-2">
+        <Form.Label>Content</Form.Label>
+        <Form.Control
+          minLength={3}
+          maxLength={500}
+          as="textarea"
+          rows={8}
+          placeholder="Enter content"
+          value={content}
+          onChange={onContentChange}
+        />
+      </Form.Group>
+      <Form.Group className="mb-2">
+        <Form.Label className="mb-0">Tags</Form.Label>
+        <Form.Text className="d-block text-muted">
+          Add up to 5 tags to describe what your question is about
+        </Form.Text>
+        <div className="d-flex flex-row mb-1">
+          {tags.map((tag) => (
+            <div
+              key={tag}
+              className="me-1 w-fit-content border rounded p-1 d-flex flex-row"
+            >
+              <div className="me-1">{tag}</div>
+              <CloseButton onClick={() => removeTag(tag)} />
+            </div>
+          ))}
+        </div>
+        <SelectSearch
+          options={getOptionTags()}
+          search
+          placeholder="Tags"
+          filterOptions={fuzzySearch}
+          onChange={onSelectTag}
+        />
+      </Form.Group>
+      {error ? (
+        <Alert className="w-fit-content p-1 mb-2" variant="danger">
+          {error}
+        </Alert>
+      ) : null}
+      <div className="d-flex justify-content-end">
+        {submitLoading ? (
+          <LoadingSpinner />
+        ) : (
+          <Button type="submit">Submit</Button>
+        )}
+      </div>
+      <Content className="mt-2" content={content} />
+    </Form>
+  );
+}
--- src/pages/NewQuestionPage.tsx
+++ src/pages/NewQuestionPage.tsx
@@ -1,13 +1,8 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Alert, Button, CloseButton, Form } from 'react-bootstrap';
 import { Navigate, useNavigate } from 'react-router-dom';
-import SelectSearch, {
-  fuzzySearch,
-  SelectedOptionValue,
-} from 'react-select-search';
 import 'react-select-search/style.css';
-import Content from '../components/Content';
 import LoadingSpinner from '../components/LoadingSpinner';
+import QuestionForm from '../components/QuestionForm';
 import QuestionService from '../service/QuestionService';
 import { AuthContext } from '../utils/auth-context';
 import ValidateUtils from '../utils/ValidateUtils';
@@ -57,7 +52,7 @@ export default function NewQuestionPage() {
   const onContentChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setContent(event.target.value);
   };
-  const onQuestionSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     const titleTrimmed = title.trim();
@@ -97,84 +92,19 @@ export default function NewQuestionPage() {
     );
   };
 
-  const getOptionTags = () => {
-    return allTags.map((tag) => ({ name: tag, value: tag }));
-  };
-
-  const onSelectTag = (tag: SelectedOptionValue | SelectedOptionValue[]) => {
-    if (typeof tag === 'string') {
-      setTags((value) => [...value, tag]);
-      setAllTags((value) => value.filter((item) => item !== tag));
-    }
-  };
-
-  const removeTag = (tag: string) => {
-    setTags((value) => value.filter((item) => item !== tag));
-    setAllTags((value) => [...value, tag]);
-  };
-
   return (
-    <Form onSubmit={onQuestionSubmit}>
-      <Form.Group>
-        <Form.Label>Title</Form.Label>
-        <Form.Control
-          minLength={3}
-          maxLength={50}
-          type="text"
-          placeholder="Enter title"
-          value={title}
-          onChange={onTitleChange}
-        />
-      </Form.Group>
-      <Form.Group className="mb-2">
-        <Form.Label>Content</Form.Label>
-        <Form.Control
-          minLength={3}
-          maxLength={500}
-          as="textarea"
-          rows={8}
-          placeholder="Enter content"
-          value={content}
-          onChange={onContentChange}
-        />
-      </Form.Group>
-      <Form.Group className="mb-2">
-        <Form.Label className="mb-0">Tags</Form.Label>
-        <Form.Text className="d-block text-muted">
-          Add up to 5 tags to describe what your question is about
-        </Form.Text>
-        <div className="d-flex flex-row mb-1">
-          {tags.map((tag) => (
-            <div
-              key={tag}
-              className="me-1 w-fit-content border rounded p-1 d-flex flex-row"
-            >
-              <div className="me-1">{tag}</div>
-              <CloseButton onClick={() => removeTag(tag)} />
-            </div>
-          ))}
-        </div>
-        <SelectSearch
-          options={getOptionTags()}
-          search
-          placeholder="Tags"
-          filterOptions={fuzzySearch}
-          onChange={onSelectTag}
-        />
-      </Form.Group>
-      {formError ? (
-        <Alert className="w-fit-content p-1 mb-2" variant="danger">
-          {formError}
-        </Alert>
-      ) : null}
-      <div className="d-flex justify-content-end">
-        {submitLoading ? (
-          <LoadingSpinner />
-        ) : (
-          <Button type="submit">Submit</Button>
-        )}
-      </div>
-      <Content className="mt-2" content={content} />
-    </Form>
+    <QuestionForm
+      onSubmit={onSubmit}
+      allTags={allTags}
+      setTags={setTags}
+      setAllTags={setAllTags}
+      title={title}
+      onTitleChange={onTitleChange}
+      content={content}
+      onContentChange={onContentChange}
+      tags={tags}
+      error={formError}
+      submitLoading={submitLoading}
+    />
   );
 }
