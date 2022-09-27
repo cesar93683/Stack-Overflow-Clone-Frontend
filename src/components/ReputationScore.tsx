2692d5dd8fd15d4d56c10d6fa279bde76994f72f
cesar93683 <48188267+cesar93683@users.noreply.github.com>
2022-09-27 16:39:40 -0700

    Update reputation to format correctly

--- src/components/ReputationScore.tsx
+++ src/components/ReputationScore.tsx
@@ -3,9 +3,20 @@ interface ReputationScoreProps {
 }
 
 export default function ReputationScore({ reputation }: ReputationScoreProps) {
+  const getReputationString = (rep: number) => {
+    const repStr = String(rep);
+    if (rep < 1000) {
+      return rep;
+    } else if (rep < 10000) {
+      return repStr[0] + ',' + repStr.substring(1);
+    } else if (rep < 100000) {
+      return repStr.substring(0, 2) + '.' + repStr[3] + 'k';
+    }
+    return repStr.substring(0, 3) + 'k';
+  };
   return (
     <span className="ms-1 fw-bold" title="reputation score " dir="ltr">
-      {reputation}
+      {getReputationString(reputation)}
     </span>
   );
 }
